cc299ada34579946e57c02484b9ec547c3414997
Eivind <eivbro@gmail.com>
2018-10-02 01:23:03 +0200

    Changes for pywal

--- user-scripts/rangerScript.sh
+++ user-scripts/rangerScript.sh
@@ -1,5 +1,5 @@
 #!bin/bash
 color=$(xrdb -query | grep '*.color0' | awk '{print $NF}')
-trans=$(more /home/broeiv/.xinitrc | grep 'wal -R' | awk '{print $NF}')
+trans=$(more $HOME/.xinitrc | grep 'wal -R' | awk '{print $NF}')
 combi='['$trans']'$color
 urxvt -bg $combi -e ranger 
